9c57232daa6839a0aa7469d9cb9e3b8dbcf7a66c
Sam <sam.shippey.oregon@gmail.com>
2017-01-14 01:05:29 -0800

    added new male names

--- script.js
+++ script.js
@@ -50,8 +50,8 @@ Array.prototype.swap = function (x,y) {
 
 //Databases
 var fnames = ["Emily", "Isabelle", "Samantha", "Alice", "Jessica", "Mary", "Kristen", "Kirsten", "Taylor"];
-var mnames = ["William", "Finn", "Luke", "Robert", "Max", "Matthew", "Chandler", "Joseph", "Stephen", "Kenneth", "Christian", "Mason", "James", "Theodore", "Franklin", "Nicholas", "Oscar", "Oswald", "Samuel"];
-var surnames = ["Carter", "Taylor", "Grant", "Knight", "Shaw", "Mason", "Mann", "Burnett", "Kinglover", "Nailo", "Chandlerson", "Kenson", "Balsiger", "Smith", "Fischer", "The Rotund", "The Great", "The Hung"];
+var mnames = ["William", "Finn", "Luke", "Robert", "Max", "Matthew", "Chandler", "Joseph", "Stephen", "Kenneth", "Christian", "Mason", "James", "Theodore", "Franklin", "Nicholas", "Oscar", "Oswald", "Samuel", "Collin", "Kent", "Alexander", "Fletcher"];
+var surnames = ["Carter", "Taylor", "Grant", "Knight", "Shaw", "Mason", "Mann", "Burnett", "Kinglover", "Nailo", "Chandlerson", "Kenson", "Balsiger", "Smith", "Fischer", "The Rotund", "The Great", "The Hung", "Houger", "Fataua", "Lyons", "Barnes"];
 var races = ["Human", "Elf", "Dwarf", "Halfling", "Half-orc", "Half-elf", "Gnome"];
 var classes = ["Barbarian", "Fighter", "Rogue", "Cleric", "Druid", "Ranger", "Paladin", "Monk", "Wizard", "Sorcerer", "Bard"];
 
@@ -306,6 +306,7 @@ while(hp<0){
 var myCharacter = new Character(myName(charsex), abilscores, charClass, charrace, charsex, myLevel, charfeats, hp, myAlignment(charClass));
 //JQUERY
 $("document").ready(function(){
+	$("holder").draggable();
 	$("#name_field").html(myCharacter.name);
 	$("#racesex_field").html(myCharacter.alignment + " " + myCharacter.sex + " " + myCharacter.race);
 	$("#class_field").html(myCharacter.classname + " " + myCharacter.bannedSchools + " " + myCharacter.level);
